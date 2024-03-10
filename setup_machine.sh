d609b8f09acf65df863d69d5ed0d132dfac733c1
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-10 14:50:56 +0530

    src: added setup machine

---
+++ setup_machine.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh 
+
+# setup homebrew first 
+
+# for making reproducible machines 
+brew install go node volta pnpm chezmoi
+
+# martian-mono font 
+brew tap homebrew/cask-fonts
+brew install --cask font-martian-mono
+
+
