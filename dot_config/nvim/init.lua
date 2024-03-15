2bf815185ef1249441cbfcdb046d162bd5e832b3
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-16 01:57:55 +0530

    update neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,19 +1,55 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
+vim.g.leader = "<M-c>"
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
-  }
+  })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-require 'base'
-require 'neovide'
-require 'keymap'
-require('lazy').setup 'plugins'
+local plugins = {
+{ 'kepano/flexoki-neovim', name = 'flexoki' },
+{
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  }
+}
+
+local opts = {}
+
+require('lazy').setup(plugins,opts)
+
+vim.cmd.colorscheme = 'flexoki-dark'
+
+local builtin  = require("telescope.builtin")
+vim.keymap.set('n','<C-p>',builtin.find_files,{}) 
+vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua","go","javascript"},
+  highlight = {enable = true },
+  indent = {enable = true},
+
+})
+
+
+
+
+
