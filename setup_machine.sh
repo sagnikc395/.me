406439ceef54f44278e3e39778c3650b529059a4
sagnik <sagnikchatterjee395@gmail.com>
2024-03-16 11:41:28 +0530

    Update setup_machine.sh
    update setup script to install via brew or asdf

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,12 +1,32 @@
 #!/bin/zsh 
 
-# setup homebrew first 
+# setup homebrew first / asdf if on linux  
+# Check if the script is running on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Installing homebrew for macOS..."
+    # Command to install file on macOS
+    # Example: cp file_for_macos /path/to/installation/directory
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    # Check if the script is running on Linux/WSL
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        echo "Installing asdf for Linux/WSL..."
+        # Command to install file on Linux/WSL
+        # Example: cp file_for_linux /path/to/installation/directory
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+        . "$HOME/.asdf/asdf.sh"
+        . "$HOME/.asdf/completions/asdf.bash"
+    else
+        echo "Unsupported operating system."
+        exit -1
+    fi
+fi
 
 # for making reproducible machines 
-brew install go node volta pnpm chezmoi
+brew install go node volta pnpm go chezmoi
 
 # martian-mono font 
 brew tap homebrew/cask-fonts
-brew install --cask font-martian-mono
+brew install font-iosevka
 
 
