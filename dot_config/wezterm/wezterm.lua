e6fdabbefc6e4a114c3e3d30872eac73304b0317
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-26 20:40:31 +0530

    src: update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,13 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "flexoki-dark"
-config.font = wezterm.font("Iosevka Term")
+config.font = wezterm.font("Monaspace Krypton")
 config.font_size = 16.5
+config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = "Github Dark (Gogh)"
+config.audible_bell = "Disabled"
+config.scrollback_lines = 10000
+config.window_close_confirmation = "NeverPrompt"
+
 
 return config
