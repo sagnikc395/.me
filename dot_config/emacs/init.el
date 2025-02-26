ea48a275c2d72cbbc5e7c32a1311d28d8dc45081
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-02-26 18:15:48 +0530

    update emacs

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -32,7 +32,12 @@
 ;;prevent emacs from creating backup files
 (setq make-backup-files nil)
 
-
+;; font size setup
+(set-face-attribute 'default nil
+		    :height 180
+		    :family "mononoki"
+		    :weight 'normal
+		    :width 'normal)
 
 ;; melpa support
 (require 'package)
@@ -47,10 +52,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(htmlize simple-httpd)))
+ '(package-selected-packages '(exec-path-from-shell haskell-mode htmlize simple-httpd)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
+
+
+;;haskell setup
+
+(use-package eglot
+  :config
+  (add-hook 'haskell-mode-hook 'eglot-ensure)
+  :custom
+  (eglot-autoshutdown t)  ;; shutdown language server after closing last file
+  (eglot-confirm-server-initiated-edits nil)  ;; allow edits without confirmation
+  )
+
+(use-package exec-path-from-shell)
+(when (daemonp)
+  (exec-path-from-shell-initialize))
+
+;; C-c C-c key to compile the haskell binary
+(use-package haskell-mode
+  :bind (:map haskell-mode-map
+              (("C-c C-c" . haskell-compile))))
+
+
+;;agda setup
